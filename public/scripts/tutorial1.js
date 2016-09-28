7a89190c4919978ae4c1876510460f18e5f755b3
Виктор Шуляк <vishulyak@sberbank.ru>
2016-09-28 19:06:18 +0700

    конец обучалки

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -2,6 +2,50 @@
 var CommentBox = React.createClass({
   displayName: 'CommentBox',
 
+  getInitialState: function() {
+    return { data: [] }
+  },
+
+  componentDidMount: function() {
+    this.loadComments();
+    setInterval(this.loadComments, this.props.pollInterval)
+  },
+
+  loadComments: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  handleCommentSubmit: function(comment) {
+    const old_comments = this.state.data;
+    comment.id = Date.now();
+    var new_comments = old_comments.concat([comment]);
+    this.setState({data: new_comments});
+
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        this.setState({data: old_comments})
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
   render: function() {
     return (
       React.createElement(
@@ -9,8 +53,8 @@ var CommentBox = React.createClass({
         {className: "commentBox"},
         "Hello, world! I am a CommentBox.",
         React.createElement('h1', { }, "Comments"),
-        React.createElement(CommentList, { data: this.props.data }),
-        React.createElement(CommentForm)
+        React.createElement(CommentList, { data: this.state.data }),
+        React.createElement(CommentForm, { onCommentSubmit: this.handleCommentSubmit})
       )
     );
   }
@@ -54,22 +98,42 @@ var Comment = React.createClass({
 
 
 var CommentForm = React.createClass({
+  getInitialState: function() {
+    return {author: "", text: ""};
+  },
+
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value})
+  },
+
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value})
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author) {
+      return ;
+    }
+    this.props.onCommentSubmit({ author: author, text: text});
+    this.setState({author: '', text: ''});
+  },
+
   render: function() {
     return (
-        <div className="commentForm">
-        Hello, world! I am a CommentForm.
-        </div>
+        <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange} />
+        <input type="text" placeholder="Say something..." value={this.state.text} onChange={this.handleTextChange} />
+        <input type="submit" value="Post" />
+        </form>
     );
   }
 });
 
 
-var data = [
-  {id: 1, author: "Pete Hunt*", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke*", text: "This is *another* comment"}
-];
-
 ReactDOM.render(
-  React.createElement(CommentBox, { data: data, url: "/api/comment" } ),
+  React.createElement(CommentBox, { url: "/api/comments", pollInterval: 2000 } ),
   document.getElementById('content')
 );
